0aa41e312248cd6bfe53e769934f5eb8bd932eea
strider <prerakmody@gmail.com>
2017-11-28 15:41:00 +0530

    Meh

---
+++ src/commands.py
@@ -0,0 +1,12 @@
+"""
+- In code folder
+floyd init pokegan
+
+- In data folder
+floyd data init pokegan
+floyd data upload
+
+- In code folder
+floyd run --cpu --mode jupyter --env tensorflow --data pmodss93/datasets/pokegan/3
+floyd run --gpu --mode jupyter --env tensorflow --data pmodss93/datasets/pokegan/3
+"""
